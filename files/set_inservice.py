d8e0f799ec0631a322f98a77fd869ce3392c120d
Stefan - Zipkid - Goethals <stefan@zipkid.eu>
2019-06-14 13:39:50 +0200

    WIP Manage required files.

---
+++ files/set_inservice.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+'''
+File managed by puppet in module profiles::service::ldap
+'''
+
+import json
+import urllib.request
+import boto3
+
+
+def mark_as_healthy(asg_client, asg_name, instance_id, result="CONTINUE"):
+    # Signal ASG that we're done
+    # Figure out which hook to signal
+    # BUG: this assumes only 1 LAUNCHING-hook is registered
+    #      possible solution: look to the CloudFormation Stack that created us
+    #      and read out the name of the hook there
+    asg_hooks = asg_client.describe_lifecycle_hooks(
+        AutoScalingGroupName=asg_name)
+
+    # Filter for EC2_INSTANCE_LAUNCHING hook
+    asg_hooks = [h for h in asg_hooks[u'LifecycleHooks']
+                 if h[u'LifecycleTransition'] == "autoscaling:EC2_INSTANCE_LAUNCHING"]
+
+    if len(asg_hooks) != 1:
+        raise RuntimeError("Could not find Lifecycle hook to notify")
+
+    asg_hooks = asg_hooks[0][u'LifecycleHookName']
+
+    asg_client.complete_lifecycle_action(
+        AutoScalingGroupName=asg_name,
+        LifecycleHookName=asg_hooks,
+        InstanceId=instance_id,
+        LifecycleActionResult=result,
+    )
+
+
+def instance_data():
+    instance_identity = urllib.request.urlopen(
+            "http://169.254.169.254/2016-09-02/dynamic/instance-identity/document"
+        ).read()
+    instance_identity = json.loads(instance_identity.decode('utf-8'))
+
+    instance_id = instance_identity['instanceId']
+    region = instance_identity['region']
+    print("I am {iid}, running in {r}".format(
+        iid=instance_id,
+        r=region,
+    ))
+
+    asg_c = boto3.client('autoscaling', region_name=region)
+    """:type : pyboto3.autoscaling"""
+
+    asg_info = asg_c.describe_auto_scaling_instances(InstanceIds=[instance_id])
+    asg = asg_info[u'AutoScalingInstances'][0][u'AutoScalingGroupName']
+    print("I am part of Auto Scaling Group {asg}".format(asg=asg))
+
+    return asg_c, asg, instance_id
+
+
+if __name__ == "__main__":
+    asg_c, asg, instance_id = instance_data()
+    mark_as_healthy(asg_c, asg, instance_id)
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,54 @@
 
 # Class: aws_lifecyle_hooks
 #
+# @param base_dir          Directory to deploy lifecycle hook scripts to.
+# @param base_requirements Requirements needed for the base tools. Array of lines to add to requirements.txt
+# @param requirements      Array of additional requirements. Array of lines to add to requirements.txt
 #
-class aws_lifecyle_hooks {
+class aws_lifecyle_hooks (  # For param defaults, see data/common.yaml
+  String        $base_dir,
+  Array[String] $base_requirements,
+  Array[String] $requirements,
+  String        $entry_script,
+){
   # resources
+  file { $base_dir:
+    ensure => directory,
+    mode   => '0755',
+  }
+
+  file { "${base_dir}/set_inservice.py":
+    ensure => file,
+    mode   => '0750',
+    source => 'puppet:///modules/aws_lifecycle_hooks/set_inservice.py',
+  }
+
+  $requirements_array = concat($base_requirements, $requirements)
+  $requirements_str = join($requirements_array, "\n")
+  file { '/opt/vrt-ldap/requirements.txt':
+    ensure  => file,
+    mode    => '0440',
+    content => $requirements_str,
+  }
+
+  # problematic cross-class dependency
+  class { '::profiles::application::python':
+    virtualenv => 'present',
+  }
+
+  python::virtualenv { $base_dir:
+    ensure       => 'present',
+    venv_dir     => "${base_dir}/venv",
+    version      => '3',
+    requirements => "${base_dir}/requirements.txt",
+  }
+
+  # hook entry point
+  $bootstrap_lifecycle_hook_cmd = "${base_dir}/venv/bin/python ${base_dir}/${entry_script}"
+  file { '/var/lib/cloud/scripts/per-boot/99_bootstrap_lifecycle_hook.sh':
+    ensure  => file,
+    mode    => '0750',
+    content => $bootstrap_lifecycle_hook_cmd,
+  }
+
 }
