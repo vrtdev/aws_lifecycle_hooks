654339fec978829efa2fb1c9a5a367e4af0ae91f
Stefan - Zipkid - Goethals <stefan@zipkid.eu>
2019-07-02 09:58:46 +0200

    Add venv & params options

--- manifests/entry_script.pp
+++ manifests/entry_script.pp
@@ -6,15 +6,25 @@ define aws_lifecycle_hooks::entry_script (
   String                            $base_dir,
   String                            $script_name,
   Integer[1]                        $index,
+  Boolean                           $use_python_venv = true,
+  Array[String]                     $parameters      = [],
   Enum['file','present','absent']   $ensure          = 'file',
 ) {
   # hook entry point
-  $bootstrap_lifecycle_hook_cmd = "#!/bin/bash\n${base_dir}/venv/bin/python ${base_dir}/${script_name}\n"
+  if $use_python_venv {
+    $cmd_init = "${base_dir}/venv/bin/python"
+  } else {
+    $cmd_init = undef
+  }
+
+  $cmd = join([$cmd_init, "${base_dir}/${script_name}", $parameters, "\n"], ' ')
+
+  $entry_script = join(['#!/bin/bash', $cmd], "\n")
 
   file { "/var/lib/cloud/scripts/per-boot/${index}_${script_name}.sh":
     ensure  => $ensure,
     mode    => '0755',
-    content => $bootstrap_lifecycle_hook_cmd,
+    content => $entry_script,
     require => Exec['aws_lifecycle_hooks : Create /var/lib/cloud/scripts/per-boot'],
   }
 }
